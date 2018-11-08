a8c6a36fcbabac51c9f146f7d6f29c2777bfe788
Anastasiia Drozdova <anastasia_drozdova@hotmail.com>
2018-11-08 15:20:25 -0500

    Changed Manifest: Main2Activity is not a laucher and paired data and time

--- Fitness354G/app/src/main/java/groupg/fitness354g/Main2Activity.java
+++ Fitness354G/app/src/main/java/groupg/fitness354g/Main2Activity.java
@@ -3,6 +3,8 @@ package groupg.fitness354g;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.util.Pair;
 import android.view.View;
 import android.widget.TextView;
 
@@ -18,11 +20,13 @@ import org.json.JSONObject;
 
 public class Main2Activity extends AppCompatActivity {
     private TextView info;
-    private  JSONObject jsonObj;
-    //private String dataObj;
+   // private  JSONObject jsonObj;
     private  String JSONString = null;
-    private String something;
+   // private String something;
     private JSONArray workouts;
+    private List<String> speed_data = new ArrayList();
+    private List<String> time_data = new ArrayList();
+    private List<Pair<String, String>> data = new ArrayList();
 
     public void callFitnessGraphView(View view)
     {
@@ -36,7 +40,7 @@ public class Main2Activity extends AppCompatActivity {
         String rawJson = loadJsonData();
 
         try {
-            something = loadEachData(rawJson);
+            loadEachData(rawJson);
         } catch (JSONException e) {
             e.printStackTrace();
         }
@@ -49,7 +53,8 @@ public class Main2Activity extends AppCompatActivity {
         String Name= intent.getStringExtra("u_id");
         String Password= intent.getStringExtra("pwd");
         //loadJsonData();
-        info.setText("Session info: "+Name +" and  "+Password + "  " + something);
+        info.setText("Session info: "+Name +" and  "+ Password + "  " + data);
+       // Log.d("TestText", data.toString());
 
     }
 
@@ -84,18 +89,22 @@ public class Main2Activity extends AppCompatActivity {
         return JSONString;
     }
 
-    public String loadEachData(String d) throws JSONException {
+    public void loadEachData(String d) throws JSONException {
         JSONObject w = new JSONObject(d);
         workouts = w.getJSONArray("data");
-        List<String> speed_data = new ArrayList();
-        List<String> time_data = new ArrayList();
+       // List<String> speed_data = new ArrayList();
+       // List<String> time_data = new ArrayList();
+       // List<Pair<String, String>> data = new ArrayList();
 
-        for (int i = 0; i< workouts.length(); i++) {
+        for (int i = 0; i< workouts.length(); i++)
+        {
             JSONObject c = workouts.getJSONObject(i);
 
             int sport = c.getInt("sport2");
+
             //only if the sport is cycling
-            if(sport == 2) {
+            if(sport == 2)
+            {
                 String speed = c.getString("speed_kmh_avg");
                 if (speed != null) {
                     speed_data.add(speed);
@@ -105,23 +114,19 @@ public class Main2Activity extends AppCompatActivity {
                 if (date != null){
                     time_data.add(date);
                 }
+                if(speed != null && date != null) {
+                    data.add(new Pair<>(date, speed));
+                }
             }
         }
 
         //print out all the workout results with for loop
         //print lists of strings
 
-
-        return speed_data.toString();
-
+        //return speed_data.toString();
 
         //return w;
-
 /*
-
-
-
-
             String time = c.getString("start_time");
             if(time != null) {
                 time_data.add(time);
