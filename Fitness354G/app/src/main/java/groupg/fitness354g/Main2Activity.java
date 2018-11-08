eb053d3924cf6b64e59153dcc5dd37db8d9c72af
Anastasiia Drozdova <anastasia_drozdova@hotmail.com>
2018-11-08 01:16:46 -0500

    Added intermediate view between login and graph, removed few warnings

--- Fitness354G/app/src/main/java/groupg/fitness354g/Main2Activity.java
+++ Fitness354G/app/src/main/java/groupg/fitness354g/Main2Activity.java
@@ -3,10 +3,17 @@ package groupg.fitness354g;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.TextView;
 
 public class Main2Activity extends AppCompatActivity {
     private TextView info;
+
+    public void callFitnessGraphView(View view)
+    {
+        Intent intent= new Intent(Main2Activity.this, FitnessGraphView.class);
+        startActivity(intent);
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -14,8 +21,8 @@ public class Main2Activity extends AppCompatActivity {
         setContentView(R.layout.activity_main2);
         info=(TextView) findViewById((R.id.textView));
         Intent intent = getIntent();
-        String Name= intent.getStringExtra("u_id");;
-        String Password= intent.getStringExtra("pwd");;
+        String Name= intent.getStringExtra("u_id");
+        String Password= intent.getStringExtra("pwd");
         info.setText("Session info: "+Name +" and  "+Password);
     }
 }
